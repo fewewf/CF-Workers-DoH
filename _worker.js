a0c96423655c1d0fdeeb0fbfb7b2994a222af21f
fewewf <32592150+fewewf@users.noreply.github.com>
2025-04-10 17:22:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,33 +6,7 @@ async function handleRequest(request) {
   const url = new URL(request.url);
   const path = url.pathname;
 
-  if (path === '/queshi') {
-    // 构建发送给 dns.google 的请求
-    const dohUrl = new URL('https://dns.google/dns-query');
-    dohUrl.search = url.search; // 复制原始请求的查询参数
-
-    const dohRequest = new Request(dohUrl.toString(), {
-      method: request.method,
-      headers: request.headers,
-      body: request.body,
-    });
-
-    // 发送请求到 dns.google 并返回响应
-    return fetch(dohRequest);
-  } else {
-    // 如果路径不是 /dns-query，则重定向到 speed.net
-    return Response.redirect('https://speed.net', 302);
-  }
-}
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
-  const url = new URL(request.url);
-  const path = url.pathname;
-
-  if (path === '/dns-query') {
+  if (path === '/dns-queshi') {
     // 构建发送给 dns.google 的请求
     const dohUrl = new URL('https://dns.google/dns-query');
     dohUrl.search = url.search; // 复制原始请求的查询参数
